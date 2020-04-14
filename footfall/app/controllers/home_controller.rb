283503718f8053e08ae23f7d12d5f5d3f10eed36
Alan Gibson <ajw.gibson@gmail.com>
2020-04-14 17:04:45 +0100

    First version of the footfall rails app

---
+++ footfall/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def dashboard
+    @title = "Dashboard"
+  end
+end
---
+++ footfall/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root to: "home#dashboard"
+
+  devise_for :users
+  as :user do
+    get 'user/edit' => 'devise/registrations#edit',   :as => 'edit_user_registration'
+    put 'user'      => 'devise/registrations#update', :as => 'user_registration'
+  end
+
+  get 'users/clear_filter'
+  resources :users
+
+  get  'profile/index'
+  get  'profile/picture'
+  post 'profile/picture', as: 'change_profile_picture', to: 'profile#picture_create'
+
+  get 'audits',             as: 'audits', to: 'audits#index'
+  get 'audits/clear_filter'
+  get 'audits/:id',         as: 'audit',  to: 'audits#show'
+end
