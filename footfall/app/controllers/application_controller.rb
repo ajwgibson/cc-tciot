3db8203c1c2254207bc34910047ce277a069297c
Alan Gibson <ajw.gibson@gmail.com>
2020-04-15 20:31:12 +0100

    Enabled paper trail whodunnit

--- footfall/app/controllers/application_controller.rb
+++ footfall/app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def user_for_paper_trail
+    user_signed_in? ? current_user.full_name : 'Not logged in'
   end
 end
