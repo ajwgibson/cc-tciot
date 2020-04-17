9cd9c14562d3216f64604384b493c4ce0db68557
Alan Gibson <ajw.gibson@gmail.com>
2020-04-17 18:32:50 +0100

    Added map to device group view

--- footfall/app/models/device_group.rb
+++ footfall/app/models/device_group.rb
@@ -14,4 +14,29 @@ class DeviceGroup < ApplicationRecord
   def device_count
     devices.count
   end
+
+  def map_centre
+    settings = Settings.current
+
+    points =
+      self
+      .devices
+      .with_a_location(true)
+      .map { |d| {latitude: d.latitude, longitude: d.longitude} }
+
+    points = [
+      {
+        latitude: settings.default_latitude,
+        longitude: settings.default_longitude
+      }
+    ] if points.count == 0
+
+    return MappingService.find_center_point(points)
+  end
+
+  def map_zoom
+    settings = Settings.current
+    devices = self.devices.with_a_location(true)
+    devices.count == 0 ? settings.default_zoom_no_location : settings.default_zoom_specific_location
+  end
 end
